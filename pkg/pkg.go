a543a79763b32a47c67f444868078dcee5a8285b
Oloruntobi1 <blgntobi@gmail.com>
2025-04-08 23:53:46 +0100

    Changes for v1.1.0

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -1,7 +1,5 @@
 package pkg
 
-import "fmt"
-
 type Seer interface {
 	See(input string) string
 }
@@ -16,34 +14,6 @@ type SeerImpl2 struct{}
 
 func (s *SeerImpl2) See(input string) string {
 	return "Processed 2: " + input
-}
-
-// --- Symbols expected to be REMOVED or UNEXPORTED in v2 ---
-
-// Function that will be removed
-func RemovedFunction(val int) string {
-	return fmt.Sprintf("Removed v1: %d", val)
-}
-
-// Type that will be removed
-type RemovedType struct {
-	ID string
-}
-
-// Constant that will be removed
-const RemovedConst = "v1_constant"
-
-// Variable that will be removed
-var RemovedVar = []string{"a", "b"}
-
-// Function that will become unexported
-func ExportedThenUnexportedFunc() {
-	fmt.Println("I am exported in v1")
-}
-
-// Type that will become unexported
-type ExportedThenUnexportedType struct {
-	Name string
 }
 
 // --- Stable Symbols (Should NOT be reported) ---
@@ -61,13 +31,13 @@ type StableType struct {
 type AType struct {
 	age   int
 	Value int
-	Place string
+	Cool  string
 }
 
 // Constant that remains the same
 const StableConst = 100
 
 // Function that will have signature changed in v2
-func ChangingFunction(s string) int {
+func ChangingFunction(s string, b bool) int {
 	return len(s)
 }
